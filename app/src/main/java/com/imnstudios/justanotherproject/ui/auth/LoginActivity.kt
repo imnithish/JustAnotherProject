e51242637941c3a24e797ae510beec27bc88d0b8
imnithish <imnithish@live.com>
2020-05-09 02:08:19 +0530

    day one

--- app/src/main/java/com/imnstudios/justanotherproject/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/imnstudios/justanotherproject/ui/auth/LoginActivity.kt
@@ -3,7 +3,7 @@ package com.imnstudios.justanotherproject.ui.auth
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.imnstudios.justanotherproject.R
 import com.imnstudios.justanotherproject.databinding.ActivityLoginBinding
 import com.imnstudios.justanotherproject.util.toast
@@ -15,7 +15,7 @@ class LoginActivity : AppCompatActivity(), AuthListener {
 
         val binding: ActivityLoginBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_login)
-        val viewModel = ViewModelProviders.of(this).get(AuthViewModel::class.java)
+        val viewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
 
         binding.viewmodel = viewModel
 
